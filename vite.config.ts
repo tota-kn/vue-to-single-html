b2b3cff6b5808b9c1b796276d60d5d52025885f5
tota-kn <nonbiriactive@gmail.com>
2025-07-28 23:14:36 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { viteSingleFile } from "vite-plugin-singlefile";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
-});
+  plugins: [vue(), viteSingleFile(), tailwindcss()],
+})
