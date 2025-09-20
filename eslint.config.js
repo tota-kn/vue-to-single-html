ed492cf92106b63c946186dc3aca49dcbf36061c
tota-kn <nonbiriactive@gmail.com>
2025-09-20 20:56:15 +0900

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,54 +1,27 @@
-import js from '@eslint/js'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
-import * as parserVue from 'vue-eslint-parser'
-import configPrettier from 'eslint-config-prettier'
-import pluginTypeScript from '@typescript-eslint/eslint-plugin'
-import * as parserTypeScript from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
 
-export default [
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default tseslint.config(
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
-
-  ...pluginVue.configs['flat/recommended'],
-  {
-    name: 'app/vue-rules',
-    files: ['**/*.vue'],
+    files: ['*.vue', '**/*.vue'],
     languageOptions: {
-      parser: parserVue,
-      parserOptions: {
-        parser: parserTypeScript,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+      parser: vueParser,
+      parserOptions: { parser: tseslint.parser, sourceType: 'module' },
     },
   },
-
-  {
-    name: 'app/typescript-rules',
-    files: ['**/*.{ts,mts,tsx}'],
-    languageOptions: {
-      parser: parserTypeScript,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': pluginTypeScript,
-    },
-    rules: {
-      ...pluginTypeScript.configs.recommended.rules,
-    },
-  },
-
-  configPrettier,
-]
+  eslintConfigPrettier,
+  includeIgnoreFile(gitignorePath)
+)
