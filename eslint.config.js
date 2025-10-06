819821decb56f62bb2beb78d591919d597498b29
tota-kn <nonbiriactive@gmail.com>
2025-10-06 13:35:32 +0000

    update

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { includeIgnoreFile } from '@eslint/compat'
+import globals from 'globals'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -20,6 +21,9 @@ export default tseslint.config(
     languageOptions: {
       parser: vueParser,
       parserOptions: { parser: tseslint.parser, sourceType: 'module' },
+      globals: {
+        ...globals.browser,
+      },
     },
   },
   eslintConfigPrettier,
--- src/pages/SamplePage.vue
+++ src/pages/SamplePage.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import HelloWorld from '../components/HelloWorld.vue'
 import LogoSection from '../components/LogoSection.vue'
+
+const result = ref({})
+const test = async () => {
+  result.value = await fetch(
+    'https://jsonplaceholder.typicode.com/todos/1'
+  ).then((response) => response.json())
+}
+
+onMounted(async () => {
+  await test()
+})
 </script>
 
 <template>
   <LogoSection />
   <HelloWorld msg="Vite + Vue Single File " />
+  <div>{{ result }}</div>
 </template>
